7e42818e77d2cadb99be607fa249fc44e56527bb
Jordi Castells <zenyor@hotmail.com>
2016-11-14 14:34:47 +0100

    constants

--- win/__sss.js
+++ win/__sss.js
@@ -1,4 +1,6 @@
 "use strict";
 exports.Win_connection_add_msg = 'open-connection_add';
 exports.Win_Icon = './win/img/logodivi.png';
+exports.HTML_Index = 'index.html';
+exports.HTML_Connection_Add = './win/connection_add.html';
 //# sourceMappingURL=__sss.js.map
